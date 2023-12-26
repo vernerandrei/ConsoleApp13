76b3650533d75a8769b73c510e7d27682b9557fb
Пользователь <@DESKTOP-E7N1RLV>
2023-12-26 19:54:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Random rnd = new Random();
+            //int number = rnd.Next(10, 99);
+            //string result = (number % 2 == 0) ? "Четное" : "Нечетное";
+            //Console.WriteLine($"Сгенерированное число: {number}. Оно {result}.");
+            //Задача 2
+            //Random rnd = new Random();
+            //int day = rnd.Next(1, 7);
+            //Console.WriteLine(day);
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine("Понедельник");break;
+            //    case 2: Console.WriteLine("Вторник"); break;
+            //    case 3: Console.WriteLine("Среда"); break;
+            //    case 4: Console.WriteLine("Четверг"); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суббота"); break;
+            //    case 7: Console.WriteLine("Воскресенье"); break;
+            //}
+            //Задача 3
+            //Console.WriteLine("Введите число a");
+            //int a = int.Parse(Console.ReadLine()); 
+            //Console.WriteLine("Введите число b");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число c");
+            //int c = int.Parse(Console.ReadLine());
+
+            //if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0)
+            //{
+            //    a /= 2;
+            //    b /= 2;
+            //    c /= 2;
+            //}
+            //else if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            //{
+            //    a *= 2;
+            //    b *= 2;
+            //    c *= 2;
+            //}
+            //else
+            //{
+            //    a += 1;
+            //    b += 1;
+            //    c += 1;
+            //}
+
+            //Console.WriteLine($"Числа после преобразования: {a}, {b}, {c}");
+            //Задача 4
+            //Console.WriteLine("Введите ваш балл:");
+            //int point = int.Parse(Console.ReadLine());
+            //if  (point >= 90 && point <= 100)
+            //    {
+            //        Console.WriteLine( "отлично");
+            //    }
+            //    else if (point >= 70 && point <= 89)
+            //    {
+            //    Console.WriteLine("хорошо");
+            //}
+            //    else if (point >= 50 && point <= 69)
+            //    {
+            //    Console.WriteLine("удовлетворительно"); ;
+            //    }
+            //    else
+            //    {
+            //    Console.WriteLine("неудовлетворительно");
+            //}
+            //Задача 5
+            //Console.Write("Введите букву");
+            //char st = char.Parse(Console.ReadLine());
+            //switch (st)
+            //{
+            //    case 'к':
+            //        Console.WriteLine("Введите радиус круга:");
+            //        double radius = Convert.ToDouble(Console.ReadLine());
+            //        double circleArea = Math.PI * Math.Pow(radius, 2);
+            //        double circlePerimeter = 2 * Math.PI * radius;
+            //        Console.WriteLine($"Площадь круга: {circleArea}, Периметр круга: {circlePerimeter}"); 
+            //    break;
+            //    case 'п':
+            //        Console.WriteLine("Введите длину прямоугольника:");
+            //        double length = Convert.ToDouble(Console.ReadLine());
+            //        Console.WriteLine("Введите ширину прямоугольника:");
+            //        double width = Convert.ToDouble(Console.ReadLine());
+            //        double rectangleArea = length * width;
+            //        double rectanglePerimeter = 2 * (length + width);
+            //        Console.WriteLine($"Площадь прямоугольника: {rectangleArea}, Периметр прямоугольника: {rectanglePerimeter}");
+            //    break;
+            //    case 'т':
+            //        Console.WriteLine("Введите длину первой стороны треугольника:");
+            //        double side1 = Convert.ToDouble(Console.ReadLine());
+            //        Console.WriteLine("Введите длину второй стороны треугольника:");
+            //        double side2 = Convert.ToDouble(Console.ReadLine());
+            //        Console.WriteLine("Введите длину третьей стороны треугольника:");
+            //        double side3 = Convert.ToDouble(Console.ReadLine());
+            //        double Perimeter = side1 + side2 + side3;
+            //        double perimeter = Perimeter / 2;
+            //        double Area = Math.Sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3));
+            //        Console.WriteLine($"Площадь треугольника: {Area}, Периметр треугольника: {Perimeter}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Неверный ввод. Пожалуйста, введите 'к' для круга, 'п' для прямоугольника или 'т' для треугольника.");
+            //        break;
+            //}
+            
+        }
+    }
+}
